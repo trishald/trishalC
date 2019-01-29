88f75dfa4d5140c1e76f9c8e03d93a76a207840d
Trishal Dhayananda <41100426+trishald@users.noreply.github.com>
2019-01-29 16:56:20 +0530

    Update quadratic.c

--- quadratic.c
+++ quadratic.c
@@ -1,65 +1,57 @@
-
 #include<stdio.h>
+#include<complex.h>
 #include<math.h>
-typedef struct complex
+#define comp complex
+void input(int *a ,int *b ,int *c)
 {
-        float real;
-        float imag;
-}comp;
-float input(float *a,float *b,float *c)
-{
-        printf("Enter a\n");
-        scanf("%f",a);
-        printf("Enter b\n");
-        scanf("%f",b);
-        printf("Enter c\n");
-        scanf("%f",c);
-        return 0;
+	printf("Enter a\n");
+	scanf("%d",a);
+	printf("Enter b\n");
+	scanf("%d",b);
+	printf("Enter c\n");
+	scanf("%d",c);
 }
-float compute(float a,float b,float c,float *d,comp *c1,comp *c2)
+float compute(int a,int b,int c,int *d,float complex *z1,float complex *z2)
 {
-float d1;
-*d=((b*b)-(4*a*c));
-d1=*d;
-if(d1==0)
-{
-        c1->real=(-b)/(2*a);
+	float d1;
+	*z1=0,*z2=0;
+	d1=(b*b)-(4*a*c);
+	if(d==0)
+	{
+		*z1=(-b)/(2*a);
+		*z2=*z1;
+	}
+	else
+	{
+		*z1=((-b)+(csqrt(d1))/(2*a));
+		*z2=((-b)-(csqrt(d1))/(2*a));
+	}
+	*d=d1;
 }
-else if(d1>0)
+void output(int d,float complex z1,float complex z2)
 {
-       c1->real=(-b+sqrt(d1))/(2*a);
-       c1->real=(-b-sqrt(d1))/(2*a);
-}
-else if(d1<0)
-{
-        c1->real=(b)/(2*a);
-        c1->imag=fabs(d1)/(2*a);
-}
-return 0;
-}
-float output(comp c1,comp c2,float d)
-{
-        if(d==0)
-        {
-              printf("Solution is real and equal\n");  
-              printf("The solution is %f \n",c1.real);
-        }
-        else if(d>0)
-        {
-                printf("The solutions are real and distinct\n");
-                printf("The solutions are %f and %f \n",c1.real,c2.real);
-        }
-        else if(d<0)
-        {
-                printf("The solutions are imaginary\n");
-                printf("%.1f+%.1fi and %.1f-%.1fi",c1.real,c1.imag,c1.real,c1.imag);
-        }
-        return 0;
+	if(d==0)
+	{
+		printf("Roots are real and equal \n");
+		printf("%.1f",creal(z1));
+	}
+	if(d>0)
+	{
+		printf("Roots are different and real\n");
+		printf("%.1f and %.1f",creal(z1),creal(z2));
+	}
+	if(d<0)
+	{
+		printf("Roots are imaginary \n");
+		printf("%.1f+%.1fi\n",creal(z1),cimag(z1));
+		printf("%.1f-%.1fi\n",creal(z2),cimag(z2));
+	}
 }
 void main()
 {
-        float a,b,c,d; comp c1,c2;
-        input(&a,&b,&c);
-        compute(a,b,c,&d,&c1,&c2);
-        output(c1,c2,d);
+	int a,b,c,d;float complex z1,z2;
+	input(&a,&b,&c);
+	compute(a,b,c,&d,&z1,&z2);
+	output(d,z1,z2);
 }
+
